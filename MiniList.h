5590f28667d93fb3be5c2d0d2a68984f38539b2b
Coder Yuan <yyqgit@126.com>
2017-01-10 23:35:24 +0800

    Reimplement list by template

--- MiniList.h
+++ MiniList.h
@@ -33,108 +33,115 @@ public:
 // TList *list: list
 // initialize a list
 
-template<typename TList> inline void listInit(TList *list) {
-  list->head = 0;
-  list->tail = 0;
-  list->size = 0;
-}
+template<typename T> class MiniList {
+private:
+  T *head;
+  T *tail;
+  unsigned int objNum;
 
-template<typename TList> inline bool listIsEmpty(TList *list) {
-  return (list->size == 0);
-}
-
-template<typename TList> inline unsigned int listSize(TList *list) {
-  return list->size;
-}
-
-template<typename TList, typename T> inline void listInsertFirst(TList *list, T *object) {
-  if(list->head != 0) {
-    object->next = list->head;
-    object->next->prev = object;
+public:
+  MiniList(): head(0), tail(0), objNum(0) {
   }
-  else {
-    object->next = 0;
-    list->tail = object;
-  }
-  object->prev = 0;
-  object->owner = list;
-  list->head = object;
 
-  list->size++;
-}
-
-template<typename TList, typename T> inline void listInsertLast(TList *list, T *object) {
-  if(list->tail != 0) {
-    object->prev = list->tail;
-    object->prev->next = object;
+  inline bool isEmpty() {
+    return (objNum == 0);
   }
-  else {
+
+  inline T* first() {
+    return head;
+  }
+
+  inline T* last() {
+    return tail;
+  }
+
+  inline unsigned int size() {
+    return objNum;
+  }
+
+  inline void insertFirst(T *object) {
+    if(head != 0) {
+      object->next = head;
+      object->next->prev = object;
+    }
+    else {
+      object->next = 0;
+      tail = object;
+    }
     object->prev = 0;
-    list->head = object;
-  }
-  object->next = 0;
-  object->owner = list;
-  list->tail = object;
+    head = object;
 
-  list->size++;
-}
-
-template<typename T> inline void listInsertAfter(T *object, T *toIns) {
-  toIns->prev = object;
-
-  if(object->next != 0) {
-    toIns->next = object->next;
-    toIns->next->prev = toIns;
-    object->next = toIns;
-  }
-  else {
-    toIns->next = 0;
-    object->owner->tail = toIns;
-    object->next = toIns;
-
-  }
-  toIns->owner = object->owner;
-
-  toIns->owner->size++;
-}
-
-template<typename T> inline void listInsertBefore(T *object, T *toIns) {
-  toIns->next = object;
-
-  if(object->prev != 0) {
-    toIns->prev = object->prev;
-    toIns->prev->next = toIns;
-    object->prev = toIns;
-  }
-  else {
-    toIns->prev = 0;
-    object->owner->head = toIns;
-    object->prev = toIns;
-
-  }
-  toIns->owner = object->owner;
-
-  toIns->owner->size++;
-}
-
-template<typename T> inline void listRemove(T *object) {
-  if(object->prev != 0) {
-    object->prev->next = object->next;
-  }
-  else {
-    // remove first object
-    object->owner->head = object->next;
+    objNum++;
   }
 
-  if(object->next != 0) {
-    object->next->prev = object->prev;
-  }
-  else {
-    // remove last object
-    object->owner->tail = object->prev;
+  inline void insertLast(T *object) {
+    if(tail != 0) {
+      object->prev = tail;
+      object->prev->next = object;
+    }
+    else {
+      object->prev = 0;
+      head = object;
+    }
+    object->next = 0;
+    tail = object;
+
+    objNum++;
   }
 
-  object->owner->size--;
-}
+  inline void insertAfter(T *object, T *newObject) {
+    newObject->prev = object;
+
+    if(object->next != 0) {
+      newObject->next = object->next;
+      newObject->next->prev = newObject;
+      object->next = newObject;
+    }
+    else {
+      newObject->next = 0;
+      tail = newObject;
+      object->next = newObject;
+
+    }
+
+    objNum++;
+  }
+
+  inline void insertBefore(T *object, T *newObject) {
+    newObject->next = object;
+
+    if(object->prev != 0) {
+      newObject->prev = object->prev;
+      newObject->prev->next = newObject;
+      object->prev = newObject;
+    }
+    else {
+      newObject->prev = 0;
+      head = newObject;
+      object->prev = newObject;
+
+    }
+
+    objNum++;
+  }
+
+  inline void remove(T *object) {
+    if(object->prev != 0) {
+      object->prev->next = object->next;
+    }
+    else {
+      head = object->next;
+    }
+
+    if(object->next != 0) {
+      object->next->prev = object->prev;
+    }
+    else {
+      tail = object->prev;
+    }
+
+    object->owner->size--;
+  }
+};
 
 #endif
--- unittests/MiniListTest.cpp
+++ unittests/MiniListTest.cpp
@@ -1,66 +1,61 @@
 #include "MiniList.h"
 #include "gtest/gtest.h"
 
-class Int;
-
-class IntList {
-public:
-  Int *head;
-  Int *tail;
-  unsigned int size;
-};
-
 class Int {
 public:
   Int *next;
   Int *prev;
-  IntList *owner;
+
   int data;
 };
 
-TEST(listInsertFirstTest, InsertFirestTestCase) {
-  IntList list;
-  Int obj;
-  Int obj1;
+TEST(initTest, initTestCase) {
+  MiniList<Int> list;
 
-  listInit(&list);  
-  listInsertFirst(&list, &obj);
-  EXPECT_EQ(list.head, &obj); 
-  EXPECT_EQ(list.tail, &obj); 
-  EXPECT_EQ(list.size, 1); 
-  EXPECT_EQ(obj.prev, (void *)0); 
-  EXPECT_EQ(obj.owner, &list); 
-
-  listInsertFirst(&list, &obj1);
-  EXPECT_EQ(list.head, &obj1); 
-  EXPECT_EQ(list.size, 2); 
-  EXPECT_EQ(obj1.prev, (void *)0); 
-  EXPECT_EQ(obj1.next, &obj); 
-  EXPECT_EQ(obj.prev, &obj1); 
-  EXPECT_EQ(obj.owner, &list); 
+  EXPECT_EQ(list.first(), (void *)0);
+  EXPECT_EQ(list.last(), (void *)0);
+  EXPECT_EQ(list.size(), 0);
+  EXPECT_EQ(list.isEmpty(), true);
 }
 
-TEST(listInsertLastTest, InsertLastTestCase) {
-  IntList list;
+TEST(insertFirstTest, insertFirstTestCase) {
+  MiniList<Int> list;
   Int obj;
   Int obj1;
 
-  listInit(&list);  
-  listInsertLast(&list, &obj);
-  EXPECT_EQ(obj.prev, (void *)0); 
-  EXPECT_EQ(list.head, &obj); 
-  EXPECT_EQ(obj.next, (void *)0); 
-  EXPECT_EQ(obj.owner, &list); 
-  EXPECT_EQ(list.tail, &obj); 
-  EXPECT_EQ(list.size, 1); 
+  list.insertFirst(&obj);
+  EXPECT_EQ(obj.next, (void *)0);
+  EXPECT_EQ(list.last(), &obj);
+  EXPECT_EQ(obj.prev, (void *)0);
+  EXPECT_EQ(list.first(), &obj);
+  EXPECT_EQ(list.size(), 1);
 
-  listInsertLast(&list, &obj1);
+  list.insertFirst(&obj1);
+  EXPECT_EQ(obj1.next, &obj);
+  EXPECT_EQ(obj.prev, &obj1);
+  EXPECT_EQ(obj1.prev, (void *)0);
+  EXPECT_EQ(list.first(), &obj1);
+  EXPECT_EQ(list.size(), 2);
+}
+
+TEST(insertLastTest, insertLastTestCase) {
+  MiniList<Int> list;
+  Int obj;
+  Int obj1;
+
+  list.insertLast(&obj);
+  EXPECT_EQ(obj.prev, (void *)0); 
+  EXPECT_EQ(list.first(), &obj);
+  EXPECT_EQ(obj.next, (void *)0); 
+  EXPECT_EQ(list.last(), &obj);
+  EXPECT_EQ(list.size(), 1);
+
+  list.insertLast(&obj1);
   EXPECT_EQ(obj1.prev, &obj); 
   EXPECT_EQ(obj.next, &obj1); 
   EXPECT_EQ(obj1.next, (void *)0); 
-  EXPECT_EQ(obj1.owner, &list); 
-  EXPECT_EQ(list.tail, &obj1); 
-  EXPECT_EQ(list.size, 2); 
+  EXPECT_EQ(list.last(), &obj1);
+  EXPECT_EQ(list.size(), 2);
 }
 
 // To do: add more unit test
